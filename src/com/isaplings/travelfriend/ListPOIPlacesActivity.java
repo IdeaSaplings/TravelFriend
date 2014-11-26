abdccb7ffd5aa034720a684a996daa9813799eed
Navine MC <mcnavine@outlook.com>
2014-11-26 18:07:07 +0530

    Refactoring ListPOIPlacesActivity

--- src/com/isaplings/travelfriend/ListPOIPlacesActivity.java
+++ src/com/isaplings/travelfriend/ListPOIPlacesActivity.java
@@ -18,24 +18,7 @@ public class ListPOIPlacesActivity extends Activity {
 
 		super.onCreate(savedInstanceState);
 
-		// Testing to show intermediate progress - As of now it is not working
-		// you may have to remove this non-working code
-		/*setTitleColor(Color.BLUE);
-
-		if (requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS)) {
-
-			setProgressBarIndeterminate(true);
-			setProgressBarIndeterminateVisibility(true);
-			// setProgressBarVisibility(true);
-
-			Log.v("Debug", "MyGPS : Progress bar set to true");
-
-		} else {
-			Log.v("Debug", "MyGPS : requestWindowFeature returned false");
-
-		}
-
-*/
+		
 		setContentView(R.layout.list_places_details);
 
 		// if you want to lock screen for always Portrait mode
@@ -47,20 +30,10 @@ public class ListPOIPlacesActivity extends Activity {
 		Location mLocation = bundle.getParcelable("LOCATION");
 		
 		
-		/*
-		 * Need not use StringExtra
-		 * 
-		 * String streetName = this.getIntent().getStringExtra("STREETNAME");
-		* String cityName = this.getIntent().getStringExtra("CITYNAME");
-		*
-		*/
-		
 		String streetName = mLocation.getExtras().getString("STREETNAME");
 		String cityName = mLocation.getExtras().getString("CITYNAME");
 				
-		//Log.v("Debug", "MyGPS : Travel-onBundle: Street Name : " + mLocation.getExtras().getString("STREETNAME"));
-		//Log.v("Debug", "MyGPS : Travel-onBundle: CityName : " + mLocation.getExtras().getString("CITYNAME"));
-
+		
 		
 		Log.v("Debug", "MyGPS : Street Name : " + streetName);
 		Log.v("Debug", "MyGPS : CityName : " + cityName);
@@ -84,10 +57,7 @@ public class ListPOIPlacesActivity extends Activity {
 				ListPOIPlacesActivity.this, this, mLocation);
 		GetMyPOITask poiTask = new GetMyPOITask(ListPOIPlacesActivity.this, fm);
 
-	/*	setProgressBarIndeterminateVisibility(false);
-		// setProgressBarVisibility(false);
-		setProgressBarIndeterminate(false);
-*/
+	
 		if (mLocation != null) {
 			// btnGetLocation.setEnabled(true);
 			Log.v(TAG,
